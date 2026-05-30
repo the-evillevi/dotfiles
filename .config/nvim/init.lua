74c88dfe6bfa880927d5acd8e001e77a48b7939c
the-evillevi <levi.carbellido@outlook.com>
2026-05-30 12:47:47 -0600

    feat(gpt5.5): small config fixed
    fix clear typos
    review and document strict uv defaults

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -722,7 +722,7 @@ require('lazy').setup({
         dependencies = {
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
-          --    https:/ithub.com/rafamadriz/friendly-snippets
+          --    https://github.com/rafamadriz/friendly-snippets
           {
             'rafamadriz/friendly-snippets',
             config = function() require('luasnip.loaders.from_vscode').lazy_load() end,
