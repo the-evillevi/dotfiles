744beba9b50a26b26289304109b91b1b1fc1a2a7
the-evillevi <levi.carbellido@outlook.com>
2026-06-06 23:09:52 -0600

    feat: modularize lazy-nvim plugin manager install

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -73,6 +73,7 @@ require 'config.options'
 -- [[ Basic Keymaps ]]
 require 'config.keymaps'
 
+-- [[ Tell nvim how to treat uncommon extensions ]]
 vim.filetype.add {
   extension = {
     astro = 'astro',
@@ -80,17 +81,7 @@ vim.filetype.add {
 }
 
 -- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then error('Error cloning lazy.nvim:\n' .. out) end
-end
-
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
+require 'config.lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
 --
